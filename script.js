6e08c7f62745f78cfd52d9310cdeec6221bf2a8c
Alexander Harris <abharris@pugetsound.edu>
2016-12-30 21:52:49 -0800

    permute rgb color function++

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-  console.log('hello')
+  // console.log('hello')
 })
 var r = 0
 var g = 0
@@ -10,6 +10,7 @@ var b = 0
 var div = $("#i0")
 
 var incrementColor = function(step){
+
   if (r < 256){
     r++
   } else if (g < 256){
@@ -17,11 +18,30 @@ var incrementColor = function(step){
   } else {
     b++
   }
-  console.log(r)
+  // console.log(r,g,b)
 }
-console.log(div)
+
+var rgbvals = []
+var permuteRGB = function(){
+  // var alpha = {r:0, g:0,b:0}
+  for (var i = 50; i < 256; i+=10) {
+    // alpha.r = i
+    for (var j = 50; j < 256; j+=10) {
+      // alpha.g = j
+      for (var k = 50; k < 256; k+=10) {
+        // alpha.b =k
+        rgbvals.push({r:i,g:j,b:k})
+      }
+    }
+  }
+}
+
+permuteRGB()
+// console.log(rgbvals[165000])
+// console.log(rgbvals)
+// console.log(div)
 var color
-for (var i = 0; i < 100; i++) {
+for (var i = 0; i < 1000; i++) {
   // var div = $("#i"+i)
   // console.log($(".container"))
   $(".section").append("<div class=\"row\"><div class=\"col s12 m12 l12\" id=\"i" +i+"\"></div></div>")
@@ -34,7 +54,7 @@ for (var i = 0; i < 100; i++) {
   // $("#i"+i).css("background-color", "#"+color)
   incrementColor(5)
   $("#i"+i).css("background-color", "rgb(" + r + "," + g + "," + b + ")")
-  // $("#i"+i).css("background-color", "rgb(255, 255, 0)")
+  $("#i"+i).css("background-color", "rgb(" +rgbvals[i].r + "," +rgbvals[i].g+","+rgbvals[i].b+ ")")
 
 
 
