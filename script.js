e3bf00535ae139f9372cf8d4b1dbe2d02a9d36ce
Alexander Harris <abharris@pugetsound.edu>
2016-12-31 12:12:13 -0800

    --extraneous, ++comments

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 
 $(document).ready(function(){
-  // console.log('hello')
 })
 var r = 0
 var g = 0
 var b = 0
-// $("#i0").css("background-color", "black")
+
 
 var div = $("#i0")
 
+// loop through colors, emphasizing the red spectrum
 var incrementColor = function(step){
-
   if (r < 256){
     r+=64
   } else if (g < 256){
@@ -18,18 +17,13 @@ var incrementColor = function(step){
   } else {
     b+=64
   }
-  // console.log(r,g,b)
 }
 
 var rgbvals = []
 var permuteRGB = function(){
-  // var alpha = {r:0, g:0,b:0}
   for (var i = 50; i < 256; i+=10) {
-    // alpha.r = i
     for (var j = 50; j < 256; j+=10) {
-      // alpha.g = j
       for (var k = 50; k < 256; k+=10) {
-        // alpha.b =k
         rgbvals.push({r:i,g:j,b:k})
       }
     }
@@ -37,26 +31,19 @@ var permuteRGB = function(){
 }
 
 permuteRGB()
-// console.log(rgbvals[165000])
-// console.log(rgbvals)
-// console.log(div)
+
 var color
-// var tuple
+
 var colorLocator = []
 var id
 for (var i = 0; i < 15; i++) {
-  // var div = $("#i"+i)
-  id = "i"+i
-  // console.log($(".container"))
-  var alpha = $(".section").append("<div class=\"row\"><div class=\"col s12 m12 l12\" id=\"i" +i+"\"></div></div>")
-  // console.log('scroll: '
-  // var alpha = $(".row")
-  // var alpha = div
-  color = (i*50).toString(16)
-  // console.log(color)
-  // alpha.css("background-color", "#"+color)
 
-  // $("#i"+i).css("background-color", "#"+color)
+  id = "i"+i
+
+  var alpha = $(".section").append("<div class=\"row\"><div class=\"col s12 m12 l12\" id=\"i" +i+"\"></div></div>")
+
+  color = (i*50).toString(16)
+
   incrementColor(5)
 
   $("#i"+i).css("background-color", "rgb(" + r + "," + g + "," + b + ")")
