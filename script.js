f20f982b5b2c06c6c4c84974676757f6ea111ee6
Alexander Harris <abharris@pugetsound.edu>
2016-12-31 12:17:03 -0800

    clean up code, add comments, include offset object literal{top,left} to the colorLocator array for each elem

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ var incrementColor = function(step){
   }
 }
 
+// permute all rgb vals, push them to the rgbvals[] array
 var rgbvals = []
 var permuteRGB = function(){
   for (var i = 50; i < 256; i+=10) {
@@ -30,28 +31,33 @@ var permuteRGB = function(){
   }
 }
 
-permuteRGB()
+// permuteRGB()
 
 var color
 
 var colorLocator = []
+
 var id
+
 for (var i = 0; i < 15; i++) {
 
+  // designate id
   id = "i"+i
 
+  // append to DOM the row, and colum for each color w/ corresponding id
   var alpha = $(".section").append("<div class=\"row\"><div class=\"col s12 m12 l12\" id=\"i" +i+"\"></div></div>")
 
-  color = (i*50).toString(16)
-
+  // call the increment rgb color function to change the values of r, g, and b
   incrementColor(5)
-
+  // next plug those cahnged values into the background color of the div
   $("#i"+i).css("background-color", "rgb(" + r + "," + g + "," + b + ")")
-  colorLocator.push({color:{r:r,g:g,b:b}, id:"#i"+i})
+
+  colorLocator.push({color:{r:r,g:g,b:b}, id:"#i"+i, offset:$("#i"+i).offset()})
   // $("#i"+i).css("background-color", "rgb(" +rgbvals[i].r + "," +rgbvals[i].g+","+rgbvals[i].b+ ")")
-  console.log('offset: ',$("#i"+i).offset())
+  // console.log('offset: ',$("#i"+i).offset())
 
   document.getElementById(id).addEventListener("click",function(){
     window.scrollTo(0,0)
   })
 }
+console.log('color locator array : ', colorLocator)
