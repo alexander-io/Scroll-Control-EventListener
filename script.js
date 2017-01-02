28693ea7e95b8e68f37d76e79e384f3b9c2ca21c
Alexander Harris <abharris@pugetsound.edu>
2017-01-01 23:01:57 -0800

    implement color control final form

--- script.js
+++ script.js
@@ -57,14 +57,14 @@ for (var i = 0; i < numColors; i++) {
   console.log('rgb to color result : ', RGB2Color(v,v+step,v+step+50))
 
   // call the increment rgb color function to change the values of r, g, and b
-  incrementColor(5)
+  // incrementColor(5)
   // next plug those cahnged values into the background color of the div
   // $("#i"+i).css("background-color", "rgb(" + r + "," + g + "," + b + ")")
   $("#i"+i).css("background-color", RGB2Color(v,v+step,v+step+50))
   $("#i"+i).css("padding", "0px")
 
 
-  colorLocator.push({color:{r:r,g:g,b:b}, id:"#i"+i, off:$("#i"+i).offset()})
+  colorLocator.push({color:RGB2Color(v,v+step,v+step+50), id:"#i"+i, off:$("#i"+i).offset()})
   // $("#i"+i).css("background-color", "rgb(" +rgbvals[i].r + "," +rgbvals[i].g+","+rgbvals[i].b+ ")")
 
   // var beta = $("body").append("<div id=\"z"+i+"\" class=\"control\" style=\"position:fixed; height:100px; width:100px; background-color:black; left:20px; top:20px;\"></div>")
@@ -90,7 +90,7 @@ for (var i = 0; i < numColors; i++) {
   // console.log('top val : ', topval)
 
   // append to the body the fixed position control divs
-  var beta = $("body").append("<div id=\"z" +i+"\" class=\"control\" style=\"position:fixed; height:"+boxheight+"px; width:100px; background-color:rgb("+colorLocator[i].color.r + "," + colorLocator[i].color.g + "," + colorLocator[i].color.b + "); left:20px; top:"+topval+"px; margin:15px;\"></div>")
+  var beta = $("body").append("<div id=\"z" +i+"\" class=\"control\" style=\"position:fixed; height:"+boxheight+"px; width:100px; background-color:"+colorLocator[i].color+ "; left:20px; top:"+topval+"px; margin:15px;\"></div>")
 }
 
 // add event listener to each controll elemnt to scrollTo the appropriate area on page
